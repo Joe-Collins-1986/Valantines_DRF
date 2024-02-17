0603103c13cec9fce72a6742947f4c9544922a9a
Her Fri <herbert.frichert@outlook.de>
2024-02-17 19:37:43 +0100

    add serializers for PartnerProfile app, add URLs, edit views

---
+++ partnerprofile/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import PartnerProfile
+
+
+
+class PartnerProfileSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the PartnerProfile model
+    Account shows object account's username in readonly format
+    Get function to set is_account to true/false
+    """
+
+    account = serializers.ReadOnlyField(source='account.username')
+    is_account = serializers.SerializerMethodField()
+
+    def get_is_account(self, obj):
+        request = self.context['request']
+        return request.user == obj.account
+
+    class Meta:
+        model = PartnerProfile
+        fields = '__all__'
+        
+
